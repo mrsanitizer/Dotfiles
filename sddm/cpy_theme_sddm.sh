58f5ec472701714e6f73ffb14ace28c8e2fab86d
mrsanitizer <114139114+mrsanitizer@users.noreply.github.com>
2025-10-30 18:33:56 +0530

    removed sddm config

--- sddm/cpy_theme_sddm.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Deploy SDDM theme from dotfiles
-
-sudo rm -rf /usr/share/sddm/themes/sddm-astronaut-theme
-sudo cp -r ~/dotfiles/sddm/sddm-astronaut-theme /usr/share/sddm/themes/
-echo "SDDM theme deployed successfully"
-
